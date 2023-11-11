a329a72565240523faffee4e2c7d96cc6da800bb
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2023-11-11 16:08:36 +1100

    -removed newline in script -script works

--- learn_page/create_pdf_links.py
+++ learn_page/create_pdf_links.py
@@ -1,6 +1,6 @@
 import os
 
-def generate_html_links(directory_path):
+def generate_html_links(directory_path, prefix):
     # Check if the provided path is a directory
     if not os.path.isdir(directory_path):
         print("Invalid directory path.")
@@ -15,12 +15,17 @@ def generate_html_links(directory_path):
     # Iterate through sorted files
     for filename, _ in sorted_files:
         # Replace whitespaces with underscores and cut the filename (excluding ".pdf") at the 28th character
-        display_filename = filename.replace(" ", "_")[:-4][:28] + ".pdf"
-        
+        shortened_filename = filename.replace(" ", "_")[:-4][:28] + ".pdf"
+        display_filename = filename[:-4]
+
+        full_filename = prefix + shortened_filename
+
         # Generate and print the HTML code
-        link = f'<a href="{display_filename}" target="_blank">{display_filename}</a>\n<br>'
+        link = f'<a href="{full_filename}" target="_blank">{display_filename}</a>'
         print(link)
 
 # Replace 'your_directory_path' with the path to your chosen directory
 your_directory_path = r'D:\GIT_REPOS\personalprojects\IoTAAWebsite\learn_page\Advocacy'
-generate_html_links(your_directory_path)
+resource_manager_path = r'/resource/resmgr/workstream_files/learn_page/'
+generate_html_links(your_directory_path, resource_manager_path)
+print(resource_manager_path)
